b0e95034e4dd5c5e88e89d526aeb8f85820d15ec
basti <sk@dotob.de>
2011-06-16 18:32:41 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lixhot::Application.config.secret_token = '6de21faf312e18ad2a61e44dc0e6c39f86100cd109a27fde1781e1d7619884023f011ff4b7f4f304b2b1ce8ca29863a40c17b1cdea2beecb6d0056bf487259e4'
