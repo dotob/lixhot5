9b89d0983824f1e7a13e26c202319a5974ae3abb
basti <sk@dotob.de>
2011-07-10 21:42:37 +0200

    consider guest count in giftlist

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,8 @@ class HomeController < ApplicationController
 	respond_to :html, :xml, :json
 
   def index
-		@somegifts = Gift.all 
+		guest_count = Guest.find_all_by_iscoming(true).count + 20
+		@somegifts = Gift.where("guest_count <= ?", guest_count) 
 		respond_with(@somegifts)
   end
 
