f13a3b1c06993f2bcb456a1de92519c8f9262967
basti <sk@dotob.de>
2011-07-15 21:15:29 +0200

    filtering of gifts perfected

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,17 @@ class HomeController < ApplicationController
 	respond_to :html, :xml, :json
 
   def index
-		guest_count = Guest.find_all_by_iscoming(true).count + 20
+		guest_count = Guest.find_all_by_iscoming(true).count + 200
 		gifts_with_no_guests = Gift.where("guest_count <= ?", guest_count).find_all{|g| g.guest.nil?} 
-		
-		@somegifts = gifts_with_no_guests#[0..5]
+
+		nodups = Set.new()				
+
+		@somegifts = Array.new()
+		for g in gifts_with_no_guests do
+			if nodups.add?(g.name)
+				@somegifts << g
+			end
+		end 
 		respond_with(@somegifts)
   end
 
