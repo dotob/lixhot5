3e9da291add2508b77e4181b058be47d0b2a790d
basti <sk@dotob.de>
2011-07-16 20:22:54 +0200

    add guestcount to gift edit form fix debug code (used 200 as guest_count in homecontroller)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 	respond_to :html, :xml, :json
 
   def index
-		guest_count = Guest.find_all_by_iscoming(true).count + 200
+		guest_count = Guest.find_all_by_iscoming(true).count + 20
 		gifts_with_no_guests = Gift.where("guest_count <= ?", guest_count).find_all{|g| g.guest.nil?} 
 
 		nodups = Set.new()				
